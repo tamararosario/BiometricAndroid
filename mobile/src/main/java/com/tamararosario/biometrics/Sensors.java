ff2c543afa38b15bcb3b17973a506c92763bfa01
Marcus <marcusmallum@gmail.com>
2017-06-01 17:08:18 +0200

    Start Sensor Class

--- mobile/src/main/java/com/tamararosario/biometrics/Sensors.java
+++ mobile/src/main/java/com/tamararosario/biometrics/Sensors.java
@@ -3,7 +3,26 @@ package com.tamararosario.biometrics;
 /**
  * Created by tamararosario on 6/1/17.
  */
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.TextView;
 
-public class Sensors {
+public class Sensors extends Activity implements SensorEventListener{
+
+    private TextView xText, yText, zText;
+    private Sensor mySensor;
+    private SensorManager SM;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+    }
+
 }
